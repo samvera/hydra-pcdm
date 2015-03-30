6778fbe56800a404357f34a5cfd6db5052b1e014
E. Lynette Rayle <elr37@cornell.edu>
2015-03-30 01:39:01 -0400

    combine module names on one line for ease of reading

--- lib/hydra/pcdm.rb
+++ lib/hydra/pcdm.rb
@@ -2,32 +2,30 @@ require 'hydra/pcdm/version'
 require 'hydra/pcdm/vocab/pcdm_terms'
 require 'active_fedora/aggregation'
 
-module Hydra
-  module PCDM
+module Hydra::PCDM
 
-    # vocabularies
-    autoload :PCDMTerms,              'hydra/pcdm/vocab/pcdm_terms'
+  # vocabularies
+  autoload :PCDMTerms,              'hydra/pcdm/vocab/pcdm_terms'
 
-    # models
-    autoload :Collection,             'hydra/pcdm/models/collection'
-    autoload :Object,                 'hydra/pcdm/models/object'
-    autoload :File,                   'hydra/pcdm/models/file'
+  # models
+  autoload :Collection,             'hydra/pcdm/models/collection'
+  autoload :Object,                 'hydra/pcdm/models/object'
+  autoload :File,                   'hydra/pcdm/models/file'
 
 
-    def self.class_from_string(class_name, container_class=Kernel)
-      container_class = container_class.name if container_class.is_a? Module
-      container_parts = container_class.split('::')
-      (container_parts + class_name.split('::')).flatten.inject(Kernel) do |mod, class_name|
-        if mod == Kernel
-          Object.const_get(class_name)
-        elsif mod.const_defined? class_name.to_sym
-          mod.const_get(class_name)
-        else
-          container_parts.pop
-          class_from_string(class_name, container_parts.join('::'))
-        end
+  def self.class_from_string(class_name, container_class=Kernel)
+    container_class = container_class.name if container_class.is_a? Module
+    container_parts = container_class.split('::')
+    (container_parts + class_name.split('::')).flatten.inject(Kernel) do |mod, class_name|
+      if mod == Kernel
+        Object.const_get(class_name)
+      elsif mod.const_defined? class_name.to_sym
+        mod.const_get(class_name)
+      else
+        container_parts.pop
+        class_from_string(class_name, container_parts.join('::'))
       end
     end
-
   end
+
 end
--- lib/hydra/pcdm/models/collection.rb
+++ lib/hydra/pcdm/models/collection.rb
@@ -1,38 +1,36 @@
-module Hydra
-  module PCDM
-    class Collection < ActiveFedora::Base
-      configure :type => RDFVocabularies::PCDMTerms.Collection  # FIX: This is how ActiveTriples sets type, but doesn't work in ActiveFedora
-      aggregates :members  # FIX: This causes error "Unknown constant Member" in collection_spec test.
+module Hydra::PCDM
+  class Collection < ActiveFedora::Base
+    configure :type => RDFVocabularies::PCDMTerms.Collection  # FIX: This is how ActiveTriples sets type, but doesn't work in ActiveFedora
+    aggregates :members  # FIX: This causes error "Unknown constant Member" in collection_spec test.
 
-      # behavior:
-      #   1) PCDM::Collection can aggregate PCDM::Collection
-      #   2) PCDM::Collection can aggregate PCDM::Object
-      #   3) PCDM::Collection can NOT aggregate non-PCDM object
-      #   4) PCDM::Collection can NOT contain PCDM::File
-      #   5) PCDM::Collection can have descriptive metadata
-      #   6) PCDM::Collection can have access metadata
-      # TODO: add code to enforce behavior rules
+    # behavior:
+    #   1) PCDM::Collection can aggregate PCDM::Collection
+    #   2) PCDM::Collection can aggregate PCDM::Object
+    #   3) PCDM::Collection can NOT aggregate non-PCDM object
+    #   4) PCDM::Collection can NOT contain PCDM::File
+    #   5) PCDM::Collection can have descriptive metadata
+    #   6) PCDM::Collection can have access metadata
+    # TODO: add code to enforce behavior rules
 
-      # TODO: Make members private so setting objects on aggregations has to go through the following methods.
+    # TODO: Make members private so setting objects on aggregations has to go through the following methods.
 
 
-      # def collections( collection )
-      #   # check that collection is an instance of PCDM::Collection
-      #   members << collection  if collection is_a? Hydra::PCDM::Collection
-      # end
-      #
-      # def objects( object)
-      #   # check that object is an instance of PCDM::Object
-      #   members << object  if object is_a? Hydra::PCDM::Object
-      # end
+    # def collections( collection )
+    #   # check that collection is an instance of PCDM::Collection
+    #   members << collection  if collection is_a? Hydra::PCDM::Collection
+    # end
+    #
+    # def objects( object)
+    #   # check that object is an instance of PCDM::Object
+    #   members << object  if object is_a? Hydra::PCDM::Object
+    # end
 
-      # TODO: RDF metadata can be added using property definitions.
-      #   * How to distinguish between descriptive and access metadata?
-      #   * Are there any default properties to set for Collection's descriptive metadata?
-      #   * Are there any default properties to set for Collection's access metadata?
-      #   * Is there a way to override default properties defined in this class?
+    # TODO: RDF metadata can be added using property definitions.
+    #   * How to distinguish between descriptive and access metadata?
+    #   * Are there any default properties to set for Collection's descriptive metadata?
+    #   * Are there any default properties to set for Collection's access metadata?
+    #   * Is there a way to override default properties defined in this class?
 
-    end
   end
 end
 
