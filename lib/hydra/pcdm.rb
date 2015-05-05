a5475de159beae87cb3462a26d08c10babf2c1e9
E. Lynette Rayle <elr37@cornell.edu>
2015-05-05 08:46:46 -0400

    move behaviors to concerns

--- lib/hydra/pcdm.rb
+++ lib/hydra/pcdm.rb
@@ -1,13 +1,15 @@
-module Hydra
-  module PCDM
+module Hydra::PCDM
 
-    # vocabularies
-    autoload :RDFVocabularies,        'hydra/pcdm/vocab/pcdm_terms'
+  # vocabularies
+  autoload :RDFVocabularies,        'hydra/pcdm/vocab/pcdm_terms'
 
-    # models
-    autoload :Collection,             'hydra/pcdm/models/collection'
-    autoload :Object,                 'hydra/pcdm/models/object'
-    autoload :File,                   'hydra/pcdm/models/file'
+  # models
+  autoload :Collection,             'hydra/pcdm/models/collection'
+  autoload :Object,                 'hydra/pcdm/models/object'
+  autoload :File,                   'hydra/pcdm/models/file'
 
-  end
+  # behavior concerns
+  autoload :CollectionBehavior,     'hydra/pcdm/models/concerns/collection_behavior'
+  autoload :ObjectBehavior,         'hydra/pcdm/models/concerns/object_behavior'
+
 end
