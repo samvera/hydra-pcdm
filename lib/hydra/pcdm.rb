132e85fbc8a6d267dfff8152cd4dd0e9d43b56d9
E. Lynette Rayle <elr37@cornell.edu>
2015-05-05 08:22:19 -0400

    move behaviors to concerns

--- lib/hydra/pcdm.rb
+++ lib/hydra/pcdm.rb
@@ -8,20 +8,8 @@ module Hydra::PCDM
   autoload :Object,                 'hydra/pcdm/models/object'
   autoload :File,                   'hydra/pcdm/models/file'
 
-
-  def self.class_from_string(class_name, container_class=Kernel)
-    container_class = container_class.name if container_class.is_a? Module
-    container_parts = container_class.split('::')
-    (container_parts + class_name.split('::')).flatten.inject(Kernel) do |mod, class_name|
-      if mod == Kernel
-        Object.const_get(class_name)
-      elsif mod.const_defined? class_name.to_sym
-        mod.const_get(class_name)
-      else
-        container_parts.pop
-        class_from_string(class_name, container_parts.join('::'))
-      end
-    end
-  end
+  # behavior concerns
+  autoload :CollectionBehavior,     'hydra/pcdm/models/concerns/collection_behavior'
+  autoload :ObjectBehavior,         'hydra/pcdm/models/concerns/object_behavior'
 
 end
---
+++ lib/hydra/pcdm/models/concerns/object_behavior.rb
@@ -0,0 +1,63 @@
+module Hydra::PCDM
+  module ObjectBehavior
+    extend ActiveSupport::Concern
+
+    included do
+      type RDFVocabularies::PCDMTerms.Object  # TODO switch to using generated vocabulary when ready
+      aggregates :members, :predicate => RDFVocabularies::PCDMTerms.hasMember, :class_name => "ActiveFedora::Base"
+    end
+
+    # TODO How to specify contains relationship for Hydra::PCDM::Object contains Hydra::PCDM::File
+
+    # behavior:
+    #   1) Hydra::PCDM::Object can NOT aggregate Hydra::PCDM::Collection
+    #   2) Hydra::PCDM::Object can aggregate Hydra::PCDM::Object
+    #   3) Hydra::PCDM::Object can contain Hydra::PCDM::File
+    #   4) Hydra::PCDM::Object can NOT aggregate non-PCDM object
+    #   5) Hydra::PCDM::Object can have descriptive metadata
+    #   6) Hydra::PCDM::Object can have access metadata
+    # TODO: add code to enforce behavior rules
+
+    # TODO: Make members private so adding to an aggregations has to go through the following methods.
+    # TODO: FIX: All of the following methods for aggregations are effected by the error "uninitialized constant Member".
+
+    def << arg
+      # check that arg is an instance of Hydra::PCDM::Object or Hydra::PCDM::File
+      raise ArgumentError, "argument must be either a Hydra::PCDM::Object or Hydra::PCDM::File" unless
+          arg.is_a?( Hydra::PCDM::Object ) || arg.is_a?( Hydra::PCDM::File )
+      members << arg  if arg.is_a? Hydra::PCDM::Object
+      files   << arg  if arg.is_a? Hydra::PCDM::File
+    end
+
+    def objects= objects
+      # check that object is an instance of Hydra::PCDM::Object
+      raise ArgumentError, "each object must be a Hydra::PCDM::Object" unless
+          objects.all? { |o| o is_a? Hydra::PCDM::Object }
+      members = objects
+    end
+
+    def objects
+      # TODO: query fedora for object id && hasMember && rdf_type == RDFVocabularies::PCDMTerms.Object
+    end
+
+    # TODO: Not sure how to handle obj1.objects << new_object.
+    #       Want to override << on obj1.objects to check that new_object is_a? Hydra::PCDM::Object
+
+
+    def contains= files
+      # check that file is an instance of Hydra::PCDM::File
+      raise ArgumentError, "each file must be a Hydra::PCDM::File" unless
+          files.all? { |f| f is_a? Hydra::PCDM::File }
+      super(files)
+    end
+
+
+    # TODO: RDF metadata can be added using property definitions.
+    #   * How to distinguish between descriptive and access metadata?
+    #   * Are there any default properties to set for Object's descriptive metadata?
+    #   * Are there any default properties to set for Object's access metadata?
+    #   * Is there a way to override default properties defined in this class?
+
+  end
+end
+
