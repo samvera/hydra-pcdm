bc1a5a271a1b069043b42f906c7686ef79528660
E. Lynette Rayle <elr37@cornell.edu>
2015-05-05 09:31:54 -0400

    fix merge mistake
    pcdm should load module Hydra

--- lib/hydra/pcdm.rb
+++ lib/hydra/pcdm.rb
@@ -1,15 +1,17 @@
-module Hydra::PCDM
+module Hydra
+  module PCDM
 
-  # vocabularies
-  autoload :RDFVocabularies,        'hydra/pcdm/vocab/pcdm_terms'
+    # vocabularies
+    autoload :RDFVocabularies,        'hydra/pcdm/vocab/pcdm_terms'
 
-  # models
-  autoload :Collection,             'hydra/pcdm/models/collection'
-  autoload :Object,                 'hydra/pcdm/models/object'
-  autoload :File,                   'hydra/pcdm/models/file'
+    # models
+    autoload :Collection,             'hydra/pcdm/models/collection'
+    autoload :Object,                 'hydra/pcdm/models/object'
+    autoload :File,                   'hydra/pcdm/models/file'
 
-  # behavior concerns
-  autoload :CollectionBehavior,     'hydra/pcdm/models/concerns/collection_behavior'
-  autoload :ObjectBehavior,         'hydra/pcdm/models/concerns/object_behavior'
+    # behavior concerns
+    autoload :CollectionBehavior,     'hydra/pcdm/models/concerns/collection_behavior'
+    autoload :ObjectBehavior,         'hydra/pcdm/models/concerns/object_behavior'
 
+  end
 end
