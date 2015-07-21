2af0bdcb0c6b950a344cc9bd5920f05c99cdb16d
Trey Terrell <trey.terrell@oregonstate.edu>
2015-07-21 11:23:51 -0700

    Adjust message

--- lib/hydra/pcdm/validators/pcdm_validator.rb
+++ lib/hydra/pcdm/validators/pcdm_validator.rb
@@ -2,7 +2,7 @@ module Hydra::PCDM::Validators
   class PCDMValidator
     def self.validate!(reflection, record)
       if !record.try(:pcdm_object?) && !record.try(:pcdm_collection?)
-        raise ActiveFedora::AssociationTypeMismatch.new "#{record} is not a PCDM object."
+        raise ActiveFedora::AssociationTypeMismatch.new "#{record} is not a PCDM object or collection."
       end
     end
   end
