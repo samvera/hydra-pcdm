06874b8d401ee36b95882431f5db311fd461c484
Trey Terrell <trey.terrell@oregonstate.edu>
2015-07-21 08:14:48 -0700

    Clean up indexer spec.

--- spec/hydra/pcdm/collection_indexer_spec.rb
+++ spec/hydra/pcdm/collection_indexer_spec.rb
@@ -2,17 +2,15 @@ require 'spec_helper'
 
 describe Hydra::PCDM::CollectionIndexer do
   let(:collection)         { Hydra::PCDM::Collection.new }
-  let(:child_collections1) { Hydra::PCDM::Collection.new(id: '123') }
-  let(:child_collections2) { Hydra::PCDM::Collection.new(id: '456') }
-  let(:child_object1)      { Hydra::PCDM::Object.new(id: '789') }
+  let(:child_collection_ids) { ['123', '456'] }
+  let(:child_object_ids) { ['789'] }
+  let(:member_ids) { ['123', '456', '789'] }
   let(:indexer)            { described_class.new(collection) }
 
   before do
-    allow(collection).to receive(:child_collections).and_return([child_collections1, child_collections2])
-    allow(collection).to receive(:child_collection_ids).and_return(collection.child_collections.map(&:id))
-    allow(collection).to receive(:child_objects).and_return([child_object1])
-    allow(collection).to receive(:child_object_ids).and_return(collection.child_objects.map(&:id))
-    allow(collection).to receive(:member_ids).and_return(['123', '456', '789'])
+    allow(collection).to receive(:child_collection_ids).and_return(child_collection_ids)
+    allow(collection).to receive(:child_object_ids).and_return(child_object_ids)
+    allow(collection).to receive(:member_ids).and_return(member_ids)
   end
 
   describe "#generate_solr_document" do
