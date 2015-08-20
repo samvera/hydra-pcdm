b81cc00314b94101b03df47af50ccb29c21425bc
Jose Blanco <blancoj@umich.edu>
2015-08-20 16:08:46 -0400

    move ancestory check from ancestory_validator to pcdm_behavior

--- lib/hydra/pcdm/validators/ancestor_validator.rb
+++ lib/hydra/pcdm/validators/ancestor_validator.rb
@@ -5,21 +5,14 @@ module Hydra::PCDM::Validators
     end
 
     attr_reader :owner, :record
-    delegate :ancestor?, to: :ancestor_checker
     def initialize(owner, record)
       @owner = owner
       @record = record
     end
 
     def validate!
-      return unless ancestor?(record)
+      return unless owner.ancestor?(record)
       fail ArgumentError, "#{record.class} with ID: #{record.id} failed to pass AncestorChecker validation"
-    end
-
-    private
-
-    def ancestor_checker
-      @ancestor_checker ||= ::Hydra::PCDM::AncestorChecker.new(owner)
     end
   end
 end
